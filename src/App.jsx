3dcd353e19396b0926da0e51a07ec3cb7198b8ac
AnastasiaKaldi <kaldianastasia@gmail.com>
2023-12-22 17:25:35 +0000

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header flex justify-center items-center h-screen">
+        The Games
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+// import "./App.css";
+
+// function NavBar() {
+//   return (
+//     <div className="App">
+//       <div className="text-purple-500">Helloooooo</div>
+//     </div>
+//   );
+// }
