859a52db0b10cf05577ae6a8e32bae481378c42f
AnastasiaKaldi <kaldianastasia@gmail.com>
2024-04-26 17:41:45 +0100

    Styling 2.05

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -21,6 +21,7 @@ function NavBar() {
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-hamburger"
           aria-expanded={isOpen}
+          style={{ marginTop: isOpen ? "-10px" : "" }}
         >
           <span className="sr-only">Open main menu</span>
           <img
